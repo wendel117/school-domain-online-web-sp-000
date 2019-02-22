187c5fe95b927e4b22370f5ecd2980523777b175
Wendy Hamilton <wdhamilton117@gmail.com>
2019-02-22 15:51:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,25 +1,25 @@
+
 class School
-  def initialize
-    @grades = Hash.new { |hash, key| hash[key] = Grade.new }
-  end
+attr_accessor :roster, :name, :grade
 
-  def add(student, grade)
-    grades[grade].add(student)
-    self
-  end
+def initialize(name)
+  @name = name
+  @roster = Hash.new
+end
 
-  def grade(grade_number)
-    grades[grade_number].db
-  end
+def name
+  @name
+end
 
-  def db
-    grades.each_with_object({}) { |(grade_number, grade), hash|
-      hash[grade_number] = grade.db
-    }
-  end
+def add_student(name, grade)
+  @roster[grade] << name
+end
 
-  def sort
-    grades.sort.each_with_object({}) { |(grade_number, grade), hash|
-      hash[grade_number] = grade.sort
-    }
-  end
+def grade
+end
+
+def sort
+end
+
+
+end
