0e2f816f7e900e77823321157e88a2447e14a211
Wendy Hamilton <wdhamilton117@gmail.com>
2019-02-22 15:32:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,5 @@
 # code here!
 class School
-attr_accessor :add_student
-attr_reader :name, :roster
 
-ROSTERS = []
-def initialize(name)
-  @name = name
-end
-
-def roster=(roster)
-  @roster = roster
-  ROSTERS << roster
-end
 
 end
