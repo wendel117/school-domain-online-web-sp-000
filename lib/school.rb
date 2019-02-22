769110dbc93f239d746fea6bfa2312f4202496cc
Wendy Hamilton <wdhamilton117@gmail.com>
2019-02-22 15:44:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,29 +1,8 @@
-require 'fis/test'
-
 
 class School
 
-  attr_accessor :roster
+def initialize(name)
+  @name = name
+  roster = []
 
-  def initialize(name)
-    @name = name
-    @roster = Hash.new { |h,k| h[k] = []}
-  end
-
-  def add_student(name, year)
-    @roster[year] << name
-  end
-
-  def grade(year)
-    @roster[year]
-  end
-
-  def sort
-    Hash[@roster.collect{ |a| [a.first, sort_students(a.first)] }]
-  end
-
-  private # cannot be called with an explicit receiver. only implicit receiver
-
-  def sort_students(year)
-   grade(year).sort
-  end
+end
